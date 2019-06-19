de82340f9f82608eb85914547e84b5a97eae5ed9
Marc McIntosh <marcmcintosh1987@gmail.com>
2019-06-19 16:17:07 +0200

    removed exess logging

--- index.js
+++ index.js
@@ -3,25 +3,19 @@ const request = require('request');
 module.exports = function hook(service) {
   const { URL, PASS, USER } = service.env;
 
-  const saveToLogs = (opts, cb) => {
-    console.log("sending :", opts, 'to: ', URL);
-
-    return request({
-      url: URL,
-      method: 'POST',
-      auth: {
-        user: USER,
-        pass: PASS,
-        sendImmediately: true,
-      },
-      json: true,
-      ...opts,
-    }, cb);
-  };
+  const saveToLogs = (opts, cb) => request({
+    url: URL,
+    method: 'POST',
+    auth: {
+      user: USER,
+      pass: PASS,
+      // sendImmediately: true,
+    },
+    json: true,
+    ...opts,
+  }, cb);
 
   const { payload, url, repository } = service.params;
-
-  console.log('Posting:', payload, 'to: ', url)
 
   return request({
     url,
@@ -31,13 +25,12 @@ module.exports = function hook(service) {
   }, (err, res) => {
     if (err) {
       console.log({ error: err.toString(), url, repository });
-      return saveToLogs({ body: { error: err, response: res } }, () => {
+      return saveToLogs({ body: { error: err.toString(), response: res } }, () => {
         service.res.end(err, 'utf8');
       });
     }
-    console.log('successfully sent hook')
+
     return saveToLogs({ body: { repository, payload, url } }, (erro) => {
-      console.log("saved log");
       if (erro) {
         console.log(erro);
         return service.res.end(erro, 'utf8');
